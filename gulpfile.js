6a811470fe743fb444a6d5ea607120eb181055cc
Nuzair Ibrahim <nuzairb@gmail.com>
2018-11-07 20:38:04 +0500

    Added minify css and livereload styles

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var livereload = require('gulp-livereload');
 var concat = require('gulp-concat');
+var minifyCss = require('gulp-minify-css');
 
 // File paths
 var DIST_PATH = 'public/dist'
@@ -13,6 +14,7 @@ gulp.task('styles', function () {
   console.log('starting styles task');
   return gulp.src(['public/css/reset.css', CSS_PATH])
     .pipe(concat('styles.css'))
+    .pipe(minifyCss())
     .pipe(gulp.dest(DIST_PATH))
     .pipe(livereload());
 });
@@ -36,9 +38,11 @@ gulp.task('default', function () {
   console.log('Starting default task');
 });
 
+// Watch
 gulp.task('watch', function () {
   console.log('Sarting watch task');
   require('./server.js');
   livereload.listen();
+  gulp.watch(CSS_PATH, ['styles']);
   gulp.watch(SCRIPTS_PATH, ['scripts']);
 });
