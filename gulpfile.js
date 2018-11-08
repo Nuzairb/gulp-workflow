80cc5ff5eb35530e85e2035de52775c7aeaa6da3
Nuzair Ibrahim <nuzairb@gmail.com>
2018-11-08 20:20:18 +0500

    compressed images

--- gulpfile.js
+++ gulpfile.js
@@ -22,11 +22,17 @@ var handlebarsLib = require('handlebars');
 var declare = require('gulp-declare');
 var wrap = require('gulp-wrap');
 
+// Image compression
+var imagemin = require('gulp-imagemin');
+var imageminPngquant = require('imagemin-pngquant');
+var imageminJpegRecompress = require('imagemin-jpeg-recompress');
+
 // File paths
 var DIST_PATH = 'public/dist';
 var SCRIPTS_PATH = 'public/scripts/**/*.js';
 var CSS_PATH = 'public/css/**/*.css';
 var TEMPLATE_PATH = 'templates/**/*.hbs';
+var IMAGES_PATH = 'public/images/**/*.{png,jpeg,jpg,svg,gif}';
 
 // // Styles
 // gulp.task('styles', function () {
@@ -127,7 +133,18 @@ gulp.task('templates', function() {
 });
 
 gulp.task('default', ['images', 'templates', 'styles', 'scripts'], function() {
-    console.log('Starting default task');
+    return gulp.src(IMAGES_PATH)
+        .pipe(imagemin(
+            [
+                imagemin.gifsicle(),
+                imagemin.jpegtran(),
+                imagemin.optipng(),
+                imagemin.svgo(),
+                imageminPngquant(),
+                imageminJpegRecompress()
+            ]
+        ))
+        .pipe(gulp.dest(DIST_PATH + '/images'));
 });
 
 gulp.task('watch', ['default'], function() {
