5e66dcd7cd038532a122474c583cd16c06b5295c
Nuzair Ibrahim <nuzairb@gmail.com>
2018-11-08 15:08:28 +0500

    init

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ var minifyCss = require('gulp-minify-css');
 var autoprefixer = require('gulp-autoprefixer');
 var plumber = require('gulp-plumber');
 var sourcemaps = require('gulp-sourcemaps');
+var sass = require('gulp-sass');
 
 // File paths
 var DIST_PATH = 'public/dist';
@@ -13,46 +14,64 @@ var SCRIPTS_PATH = 'public/scripts/**/*.js';
 var CSS_PATH = 'public/css/**/*.css';
 
 // Styles
-gulp.task('styles', function () {
-	console.log('starting styles task');
-	return gulp.src(['public/css/reset.css', CSS_PATH])
-		.pipe(plumber(function (err) {
-			console.log('Styles Task Error');
-			console.log(err);
-			this.emit('end');
-		}))
-		.pipe(sourcemaps.init())
-		.pipe(autoprefixer())
-		.pipe(concat('styles.css'))
-		.pipe(minifyCss())
-		.pipe(sourcemaps.write())
-		.pipe(gulp.dest(DIST_PATH))
-		.pipe(livereload());
+// gulp.task('styles', function() {
+//     console.log('starting styles task');
+//     return gulp.src(['public/css/reset.css', CSS_PATH])
+//         .pipe(plumber(function(err) {
+//             console.log('Styles Task Error');
+//             console.log(err);
+//             this.emit('end');
+//         }))
+//         .pipe(sourcemaps.init())
+//         .pipe(autoprefixer())
+//         .pipe(concat('styles.css'))
+//         .pipe(minifyCss())
+//         .pipe(sourcemaps.write())
+//         .pipe(gulp.dest(DIST_PATH))
+//         .pipe(livereload());
+// });
+
+// Styles Scss 
+gulp.task('styles', function() {
+    console.log('starting styles task');
+    return gulp.src('public/scss/style.scss')
+        .pipe(plumber(function(err) {
+            console.log('Styles Task Error');
+            console.log(err);
+            this.emit('end');
+        }))
+        .pipe(sourcemaps.init())
+        .pipe(autoprefixer())
+        .pipe(sass())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(DIST_PATH))
+        .pipe(livereload());
 });
 
-// Scripts
-gulp.task('scripts', function () {
-	console.log('starting scripts task');
 
-	return gulp.src(SCRIPTS_PATH)
-		.pipe(uglify())
-		.pipe(gulp.dest(DIST_PATH))
-		.pipe(livereload());
+// Scripts
+gulp.task('scripts', function() {
+    console.log('starting scripts task');
+
+    return gulp.src(SCRIPTS_PATH)
+        .pipe(uglify())
+        .pipe(gulp.dest(DIST_PATH))
+        .pipe(livereload());
 });
 
 // Images
-gulp.task('images', function () {
-	console.log('starting images task');
+gulp.task('images', function() {
+    console.log('starting images task');
 });
 
-gulp.task('default', function () {
-	console.log('Starting default task');
+gulp.task('default', function() {
+    console.log('Starting default task');
 });
 
-gulp.task('watch', function () {
-	console.log('Starting watch task');
-	require('./server.js');
-	livereload.listen();
-	gulp.watch(SCRIPTS_PATH, ['scripts']);
-	gulp.watch(CSS_PATH, ['styles']);
+gulp.task('watch', function() {
+    console.log('Starting watch task');
+    require('./server.js');
+    livereload.listen();
+    gulp.watch(SCRIPTS_PATH, ['scripts']);
+    gulp.watch(CSS_PATH, ['styles']);
 });
